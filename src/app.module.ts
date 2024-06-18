ddb0ffc97defb315120bb464ac2e42a2a076d3a2
Theo Ojochegbe <symplytheo@gmail.com>
2024-06-18 11:10:39 +0100

    chore: setup basic file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
+import { ResourcesModule } from './resources/resources.module';
+import { AdminModule } from './admin/admin.module';
+import { ProductsModule } from './products/products.module';
+import { NuggetsModule } from './nuggets/nuggets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    EventsModule,
+    ResourcesModule,
+    AdminModule,
+    ProductsModule,
+    NuggetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
