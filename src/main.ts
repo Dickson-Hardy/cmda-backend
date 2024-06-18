ff84d2bb02762d02d6a41870e271ff12aa543dba
Theo Ojochegbe <symplytheo@gmail.com>
2024-06-18 11:10:22 +0100

    chore: setup swagger for api docss

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CMDA Nigeria API')
+    .setDescription('API documentation for CMDA Nigeria')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('apidocs', app, swaggerDoc);
+  //
   await app.listen(3000);
 }
 bootstrap();
