c5d375c2b6ca3a968e2a2a0344d80e090e1f6eed
Theo Ojochegbe <symplytheo@gmail.com>
2024-06-23 14:27:07 +0100

    prod: cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
   // Swagger configuration
